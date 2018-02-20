4dd953f35979aa86acc903117e9ccc43cce5e30e
Naseka Andrew <andrew.naseka@yandex.ru>
2018-02-19 22:04:43 -0900

    Добавлена базовая структура терхадресного кода

---
+++ Compiler.ThreeAddressCode/TANode.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.ThreeAddressCode
+{
+    /*
+     * =========================================================
+     * Трехадресный код имеет следующий вид:
+     * Result := Left Operation Right
+     * Result, Left, Right - операнды
+     * Operation - бинарная операция 
+     * =========================================================
+    */
+
+    /// <summary>
+    /// Базовый класс для трехадресного кода
+    /// </summary>
+    public abstract class TANode
+    {
+        public abstract TAExpr Left { get; set; }
+        public abstract TAExpr Right { get; set; }
+        public abstract TAExpr Result { get; set; }
+        public abstract string Operation { get; set; }
+    }
+
+    /*
+     * =========================================================
+     * Операнды могут быть либо константами,
+     * либо сгенерированными переменными
+     * =========================================================
+    */
+
+    /// <summary>
+    /// Базовый класс для операндов
+    /// </summary>
+    public class TAExpr
+    {
+    }
+
+    /// <summary>
+    /// Операнд-константа (числа типа int)
+    /// </summary>
+    public class IntConst : TAExpr
+    {
+        public int Num { get; set; }
+        public IntConst(int num) { Num = num; }
+    }
+
+    /// <summary>
+    /// Операнд-временного имени
+    /// </summary>
+    public class GenName : TAExpr
+    {
+        public string Name { get; set; }
+        public GenName(string name) { Name = name; }
+    }
+
+    /*
+     * =========================================================
+     * Код программы храниться в виде команд 
+     * в формате трехадресного кода в массиве
+     * =========================================================
+    */
+
+    /// <summary>
+    /// Класс для хранения программы в формате 
+    /// трехадресного кода
+    /// </summary>
+    public class TACode
+    {
+        List<TANode> table;
+    }
+}
--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Compiler.Parser;
-using Compiler.Parser.AST;
 using System.IO;
+using Compiler.Parser;
 
 namespace Compiler
 {
