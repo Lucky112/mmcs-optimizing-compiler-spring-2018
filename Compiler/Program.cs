724fcf76e9aea12caf0494c5e7bd099a96c9fccc
rekrut <tananakin.mihail@yandex.ru>
2018-02-25 23:57:35 +0300

    Исполняемый файл изменён для тестирования новых классов

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -6,13 +6,86 @@ using System.Threading.Tasks;
 using System.IO;
 using Compiler.Parser;
 
+using Compiler.ThreeAddrCode.Nodes;
+using Compiler.ThreeAddrCode.Expressions;
+using Compiler.ThreeAddrCode.CFG;
+using Compiler.ThreeAddrCode;
+
 namespace Compiler
 {
     class Program
     {
         public static void Main()
         {
-            Console.OutputEncoding = Encoding.UTF8;
+            TACode ta_code = new TACode();
+
+            Assign ass1 = new Assign();
+            ass1.Left = new IntConst(3);
+            ass1.Operation = OpCode.Minus;
+            ass1.Right = new IntConst(5);
+            ass1.Result = new Var();
+
+            Assign ass2 = new Assign();
+            ass2.Left = new IntConst(10);
+            ass2.Operation = OpCode.Plus;
+            ass2.Right = new IntConst(2);
+            ass2.Result = new Var();
+
+            Assign ass3 = new Assign();
+            ass3.Operation = OpCode.Minus;
+            ass3.Right = new IntConst(1);
+            ass3.Result = new Var();
+
+            IfGoto ifgt1 = new IfGoto();
+            ifgt1.Condition = new IntConst(1);
+            ifgt1.TargetLabel = ass3.Label;
+            ass3.IsLabeled = true; //На этот оперетор мы переходим по условию 
+
+            Assign ass4 = new Assign();
+            ass4.Left = ass3.Result;
+            ass4.Operation = OpCode.Plus;
+            ass4.Right = new IntConst(1999);
+            ass4.Result = new Var();
+
+            IfGoto ifgt2 = new IfGoto();
+            ifgt2.Condition = new IntConst(2);
+            ifgt2.TargetLabel = ass2.Label;
+            ass2.IsLabeled = true; //На этот оперетор мы переходим по условию 
+
+            Assign ass5 = new Assign();
+            ass5.Left = new IntConst(7);
+            ass5.Operation = OpCode.Mul;
+            ass5.Right = new IntConst(4);
+            ass5.Result = new Var();
+
+            Assign ass6 = new Assign();
+            ass6.Left = new IntConst(100);
+            ass6.Operation = OpCode.Div;
+            ass6.Right = new IntConst(25);
+            ass6.Result = new Var();
+
+            ta_code.AddNode(ass1);
+            ta_code.AddNode(ass2);
+            ta_code.AddNode(ass3);
+            ta_code.AddNode(ifgt1);
+            ta_code.AddNode(ass4);
+            ta_code.AddNode(ifgt2);
+            ta_code.AddNode(ass5);
+            ta_code.AddNode(ass6);
+
+            string str_code = ta_code.ToString();
+            Console.WriteLine(str_code);
+
+            List<BasicBlock> b_blocks = BasicBlock.CreateBasicBlockList(ta_code);
+            
+            foreach (var bbl in b_blocks)
+            {
+                Console.WriteLine("Block[" + bbl.BlockId + "]");
+                string bbl_code_str = bbl.CodeList.Aggregate("", (s, node) => s + node.ToString() + Environment.NewLine);
+                Console.WriteLine(bbl_code_str);
+            }
+
+            /**Console.OutputEncoding = Encoding.UTF8;
             string FileName = @"..\..\sample.txt";
             try
             {
@@ -46,7 +119,7 @@ namespace Compiler
                 Console.WriteLine("Синтаксическая ошибка. " + e.Message);
             }
 
-            Console.ReadLine();
+            Console.ReadLine();**/
         }
     }
 }
