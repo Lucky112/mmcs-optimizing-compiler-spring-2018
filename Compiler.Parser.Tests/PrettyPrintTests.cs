8b0656cffb5c77faa669507f97ec807a0655ad35
Alexander Светлый <asvetly1337@gmail.com>
2018-04-03 11:18:27 +0300

    Добавлен проект для тестирования парсера

---
+++ Compiler.Parser.Tests/PrettyPrintTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace Compiler.Parser.Tests
+{
+    [TestFixture()]
+    public class PrettyPrintTests
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
