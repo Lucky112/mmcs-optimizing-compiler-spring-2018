1eff2c0cb108d0db2bc4cf825d1a46bfee9b9353
alter <al.ter0124@gmail.com>
2018-04-05 15:35:24 +0300

    Добавила итерационный алгоритм для распространения констант

--- Compiler.ThreeAddrCode/DFA/ConstantPropagation/IterativeAlgorithm.cs
+++ Compiler.ThreeAddrCode/DFA/ConstantPropagation/IterativeAlgorithm.cs
@@ -11,7 +11,32 @@ namespace Compiler.ThreeAddrCode.DFA.ConstantPropagation
     {
         public InOutData<Dictionary<Guid, VarValue>> Analyze(ControlFlowGraph graph, ILatticeOperations<Dictionary<Guid, VarValue>> ops, ITransferFunction<Dictionary<Guid, VarValue>> f)
         {
-            return null;
+            var data = new InOutData<Dictionary<Guid, VarValue>>
+            {
+                [graph.CFGNodes.ElementAt(0)] = (
+        ops.Lower,
+        f.Transfer(graph.CFGNodes.ElementAt(0), ops.Lower, ops)
+    )
+            };
+            foreach (var node in graph.CFGNodes.Skip(1))
+                data[node] = (ops.Lower, ops.Lower);
+            var outChanged = true;
+            while (outChanged)
+            {
+                outChanged = false;
+                foreach (var block in graph.CFGNodes.Skip(1))
+                {
+                    var inset = block.Parents.Aggregate(ops.Lower, (x, y)
+                        => ops.Operator(x, data[y].Item2));
+                    var outset = f.Transfer(block, inset, ops);
+                    if (outset == data[block].Item2)
+                    {
+                        outChanged = true;
+                        data[block] = (inset, outset);
+                    }
+                }
+            }
+            return data;
         }
     }
 }
