78e1843bac32368639c00ddf94d9f46dc5bac9ae
Alexey Goncharov <darin100@yandex.com>
2018-05-05 18:36:59 +0300

    Добавил класс для нумерации узлов в графе

---
+++ Compiler.ThreeAddrCode/CFG/GraphNumerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.ThreeAddrCode.CFG
+{
+    public class GraphNumerator : IGraphNumerator
+    {
+        public static GraphNumerator BackOrder(ControlFlowGraph graph)
+        {
+            var root = graph.CFGNodes.ElementAt(0);
+            var num = new GraphNumerator();
+            var index = 0;
+
+            void Iter(BasicBlock node)
+            {
+                var children = node.Children;
+                foreach(var c in children)
+                    Iter(node);
+                num._num[node] = index;
+                index++;
+            }
+
+            return num;
+        }
+
+        private readonly Dictionary<BasicBlock, int> _num = new Dictionary<BasicBlock, int>();
+
+        public int? GetIndex(BasicBlock b) 
+            => _num.TryGetValue(b, out var res) ?
+                new int?(res) : null;
+    }
+
+    public interface IGraphNumerator
+    {
+        int? GetIndex(BasicBlock b);
+    }
+}
