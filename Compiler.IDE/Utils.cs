dc5fc89ee85c51fa26bc999ad0b261984d718c57
Alexander Birukov <sanya.rnd@gmail.com>
2018-05-06 03:48:29 +0300

    вспомогательный класс для общих расчетов

---
+++ Compiler.IDE/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Compiler.IDE
+{
+    static class Utils
+    {
+        static double SCALE_MIN = 0.1;
+        static double SCALE_MAX = 1;
+
+        public static double TrackBarToScale(TrackBar bar)
+        {
+            return MapToRange(bar.Value, bar.Minimum, bar.Maximum, SCALE_MIN, SCALE_MAX);
+        }
+
+        public static double MapToRange(double x, double in_min, double in_max, double out_min, double out_max)
+        {
+            return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        }
+
+        public static Image ScaleImage(Image original, double scaleValue)
+        {
+            Size size = new Size((int)(original.Width * scaleValue), (int)(original.Height * scaleValue));
+            return new Bitmap(original, size);
+        }
+    }
+}
